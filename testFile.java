c700b3e0971104338e36bbf3b8d38788d30d57ad
EricTonygo <tonye.eric@gmail.com>
2018-09-13 13:42:31 +0200

    Change of file name

--- testFile.java
+++ testFile.java
@@ -1,5 +1,5 @@
 public class TestFile{
 	public static void main(String[] args){
-		System.out.println("Exécution d'un projet Jenkins!")
+		System.out.println("Exécution d'un projet Jenkins!");
 	}
 }
